1f37b795e10b10ef8326b03512451fe501ec9c39
plastik-punk <e12023150@student.tuwien.ac.at>
2024-07-24 22:59:27 +0200

    general frontend design

--- frontend/src/app/components/app/app.component.ts
+++ frontend/src/app/components/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,43 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  currentRoute: string = '';
+  navHighlight= 0;  // 0 = projects, 1 = tn, 2 = profile
+  name = 'Jörg Zwicker';
+  subtitle = 'Übersicht';
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  ngOnInit() {
+    this.currentRoute = this.router.url;
+    this.getCurrentName()
+    this.getCurrentPage()
+    this.getCurrentSubtitle()
+  }
+
+  getCurrentName() {
+
+  }
+
+  getCurrentPage(): void {
+    if (this.currentRoute.includes('projects')) {
+      this.navHighlight = 0;
+    }
+    else if (this.currentRoute.includes('tn')) {
+      this.navHighlight = 1;
+    }
+    else if (this.currentRoute.includes('profile')) {
+      this.navHighlight = 2;
+    }
+  }
+
+  getCurrentSubtitle() {
+
+  }
 }
